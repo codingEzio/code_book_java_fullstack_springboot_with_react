f399878cf7609bc74ad16ebf540675c740077a95
!Mensch <assassinste@gmail.com>
2022-07-29 12:38:47 +0800

    Adding logging functionality

--- starting/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ starting/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,10 +1,15 @@
 package com.packt.cardatabase;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
 	public static void main(String[] args) {
 
@@ -12,6 +17,7 @@ public class CardatabaseApplication {
 				CardatabaseApplication.class,
 				args
 		);
+		logger.info("Slf4j says hello!");
 	}
 
 }

4b19358653d90250379955692808b08abd77626b
!Mensch <assassinste@gmail.com>
2022-07-30 20:04:43 +0800

    Preloading demo data

--- starting/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ starting/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -8,9 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
+
+	@Autowired
+	private CarRepository carRepository;
 
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
@@ -27,7 +34,12 @@ public class CardatabaseApplication {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			// pass
+			carRepository.save(new Car("Ford", "Mustang", "Red",
+					"ADF-1121", 2017, 59000));
+			carRepository.save(new Car("Nissan", "Leaf", "White",
+					"SSJ-3002", 2014, 29000));
+			carRepository.save(new Car("Toyota", "Prius", "Silver",
+					"KKO-0212", 2018, 39000));
 		};
 	}
 

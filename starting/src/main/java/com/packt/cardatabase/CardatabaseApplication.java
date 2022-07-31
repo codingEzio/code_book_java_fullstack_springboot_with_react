bb0f85027337be62d0e6395b6251016c6d5dada6
!Mensch <assassinste@gmail.com>
2022-07-31 10:10:39 +0800

    Preloading demo data in accordance with the structural changes

--- starting/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ starting/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -12,12 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 
 @SpringBootApplication
 public class CardatabaseApplication {
 
 	@Autowired
 	private CarRepository carRepository;
+
+	@Autowired
+	private OwnerRepository ownerRepository;
 
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 
@@ -34,12 +39,17 @@ public class CardatabaseApplication {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
+			Owner owner1 = new Owner("John", "Doe");
+			Owner owner2 = new Owner("Dua", "Lipa");
+			ownerRepository.save(owner1);
+			ownerRepository.save(owner2);
+
 			carRepository.save(new Car("Ford", "Mustang", "Red",
-					"ADF-1121", 2017, 59000));
+					"ADF-1121", 2017, 59000, owner1));
 			carRepository.save(new Car("Nissan", "Leaf", "White",
-					"SSJ-3002", 2014, 29000));
+					"SSJ-3002", 2014, 29000, owner1));
 			carRepository.save(new Car("Toyota", "Prius", "Silver",
-					"KKO-0212", 2018, 39000));
+					"KKO-0212", 2018, 39000, owner2));
 		};
 	}
 

d9e0a4cef8920c6b308ecb90e79e9651d6be9580
!Mensch <assassinste@gmail.com>
2022-08-28 14:02:30 +0800

    dev: Preload test data for Entity 'Car'

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -7,11 +7,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.packt.cardatabase.domain.Car;
+import com.packt.cardatabase.domain.CarRepository;
+
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(
 			CardatabaseApplication.class
 	);
+
+	@Autowired
+	private CarRepository carRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -21,6 +29,12 @@ public class CardatabaseApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// pass
+		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 59000, 2021));
+		carRepository.save(new Car("Ford", "Future", "White", "BEA-9221", 39312, 2020));
+		carRepository.save(new Car("Geely", "Redemption", "Red", "PAC-1241", 12553, 2021));
+		carRepository.save(new Car("Tesla", "Onward", "White", "POI-1422", 37461, 2019));
+		carRepository.save(new Car("Eurus", "Eastwind", "Green", "MJK-3214", 41422, 2020));
+		carRepository.save(new Car("Olive", "Anniversary", "White", "ERQ-2411", 15132, 2022));
+		carRepository.save(new Car("Future", "TotalEV", "Black", "JOK-1242", 64235, 2020));
 	}
 }

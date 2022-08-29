9e3371cf2690d9ddbe026bb99f648ff4c5060a73
!Mensch <assassinste@gmail.com>
2022-08-29 11:23:36 +0800

    dev: Removing old test data (haven't figured out how to make the new structure work)

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -36,29 +36,6 @@ public class CardatabaseApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Owner ownerAlex = new Owner("Alex", "Cameron");
-		Owner ownerMosh = new Owner("Mosh", "Annie");
-
-		ownerRepository.saveAll(Arrays.asList(ownerAlex, ownerMosh));
-
-		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 59000, 2021, ownerAlex));
-		carRepository.save(new Car("Ford", "Future", "White", "BEA-9221", 39312, 2020, ownerAlex));
-		carRepository.save(new Car("Geely", "Redemption", "Red", "PAC-1241", 12553, 2021, ownerMosh));
-		carRepository.save(new Car("Tesla", "Onward", "White", "POI-1422", 37461, 2019, ownerAlex));
-		carRepository.save(new Car("Eurus", "Eastwind", "Green", "MJK-3214", 41422, 2020, ownerAlex));
-		carRepository.save(new Car("Olive", "Anniversary", "White", "ERQ-2411", 15132, 2022, ownerMosh));
-		carRepository.save(new Car("Future", "TotalEV", "Black", "JOK-1242", 64235, 2020, ownerMosh));
-
-		for (Car car: carRepository.findByColor("White")) {
-			logger.info("White Cars -> " + car.getBrand() + " " + car.getModel());
-		}
-
-		for (Car car: carRepository.findByReleasedAt(2020)) {
-			logger.info("Released at 2020 -> " + car.getBrand() + " " + car.getModel());
-		}
-
-		for (Car car: carRepository.findByColorAndReleasedAt("White", 2020)) {
-			logger.warn("White and 2020 -> " + car.getBrand() + " " + car.getModel());
-		}
+		// pass
 	}
 }

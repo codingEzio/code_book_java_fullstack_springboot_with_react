775706389c81bacd7ea296d92954c970b2223188
!Mensch <assassinste@gmail.com>
2022-08-29 10:54:51 +0800

    dev: Rewrote and preload the test data for Entity 'Car' and 'Owner'

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -11,6 +11,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CardatabaseApplication implements CommandLineRunner {
@@ -21,6 +25,9 @@ public class CardatabaseApplication implements CommandLineRunner {
 	@Autowired
 	private CarRepository carRepository;
 
+	@Autowired
+	private OwnerRepository ownerRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
 
@@ -29,13 +36,18 @@ public class CardatabaseApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 59000, 2021));
-		carRepository.save(new Car("Ford", "Future", "White", "BEA-9221", 39312, 2020));
-		carRepository.save(new Car("Geely", "Redemption", "Red", "PAC-1241", 12553, 2021));
-		carRepository.save(new Car("Tesla", "Onward", "White", "POI-1422", 37461, 2019));
-		carRepository.save(new Car("Eurus", "Eastwind", "Green", "MJK-3214", 41422, 2020));
-		carRepository.save(new Car("Olive", "Anniversary", "White", "ERQ-2411", 15132, 2022));
-		carRepository.save(new Car("Future", "TotalEV", "Black", "JOK-1242", 64235, 2020));
+		Owner ownerAlex = new Owner("Alex", "Cameron");
+		Owner ownerMosh = new Owner("Mosh", "Annie");
+
+		ownerRepository.saveAll(Arrays.asList(ownerAlex, ownerMosh));
+
+		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 59000, 2021, ownerAlex));
+		carRepository.save(new Car("Ford", "Future", "White", "BEA-9221", 39312, 2020, ownerAlex));
+		carRepository.save(new Car("Geely", "Redemption", "Red", "PAC-1241", 12553, 2021, ownerMosh));
+		carRepository.save(new Car("Tesla", "Onward", "White", "POI-1422", 37461, 2019, ownerAlex));
+		carRepository.save(new Car("Eurus", "Eastwind", "Green", "MJK-3214", 41422, 2020, ownerAlex));
+		carRepository.save(new Car("Olive", "Anniversary", "White", "ERQ-2411", 15132, 2022, ownerMosh));
+		carRepository.save(new Car("Future", "TotalEV", "Black", "JOK-1242", 64235, 2020, ownerMosh));
 
 		for (Car car: carRepository.findByColor("White")) {
 			logger.info("White Cars -> " + car.getBrand() + " " + car.getModel());

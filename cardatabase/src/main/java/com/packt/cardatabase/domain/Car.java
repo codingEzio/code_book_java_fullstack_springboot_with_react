d640de26feee322e6fcac735c31034ddaec53748
!Mensch <assassinste@gmail.com>
2022-08-29 10:53:06 +0800

    dev: Defining the (one specific) 'owner' to Entity 'Car'(s)

--- cardatabase/src/main/java/com/packt/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/Car.java
@@ -1,30 +1,39 @@
 package com.packt.cardatabase.domain;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import javax.persistence.ManyToOne;
+import javax.persistence.JoinColumn;
+
 @Entity
 public class Car {
 	@Id
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long id;
 
 	private String brand, model, color, registerNumber;
 	private int price, releasedAt;
 
-	public Car () {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "owner")
+	private Owner owner;
+
+	public Car() {
 	}
 
-	public Car (String brand, String model, String color, String registerNumber,
-	            int price, int releasedAt) {
+	public Car(String brand, String model, String color, String registerNumber,
+	           int price, int releasedAt, Owner owner) {
 		this.brand = brand;
 		this.model = model;
 		this.color = color;
 		this.registerNumber = registerNumber;
 		this.price = price;
 		this.releasedAt = releasedAt;
+		this.owner = owner;
 	}
 
 	public long getId() {
@@ -81,5 +90,13 @@ public class Car {
 
 	public void setReleasedAt(int releasedAt) {
 		this.releasedAt = releasedAt;
+	}
+
+	public Owner getOwner() {
+		return owner;
+	}
+
+	public void setOwner(Owner owner) {
+		this.owner = owner;
 	}
 }

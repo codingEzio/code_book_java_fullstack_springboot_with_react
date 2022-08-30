4464667638c1518fcd9a23b0761273187a6b65bc
!Mensch <assassinste@gmail.com>
2022-08-30 17:51:41 +0800

    feat: Configuring request parameter names

--- cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.repository.query.Param;
 
 @RepositoryRestResource(path = "cars")
 public interface CarRepository extends CrudRepository<Car, Long> {
 
-	List<Car> findByColor(String color);
+	List<Car> findByColor(@Param("color") String color);
 
-	List<Car> findByReleasedAt(int year);
+	List<Car> findByReleasedAt(@Param("year") int year);
 
-	List<Car> findByColorAndReleasedAt(String color, int year);
+	List<Car> findByColorAndReleasedAt(@Param("color") String color, @Param("year") int year);
 }

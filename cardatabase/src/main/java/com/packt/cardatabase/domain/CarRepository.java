b233496b28a327455bd24d900a0731cb927a448a
!Mensch <assassinste@gmail.com>
2022-08-30 11:53:18 +0800

    dev: Explicitly specifying the API routes for Entity 'Car' and 'Owner'

--- cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/CarRepository.java
@@ -3,7 +3,9 @@ package com.packt.cardatabase.domain;
 import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "cars")
 public interface CarRepository extends CrudRepository<Car, Long> {
 
 	List<Car> findByColor(String color);
--- cardatabase/src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
+++ cardatabase/src/main/java/com/packt/cardatabase/domain/OwnerRepository.java
@@ -1,6 +1,8 @@
 package com.packt.cardatabase.domain;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path = "owners")
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 }

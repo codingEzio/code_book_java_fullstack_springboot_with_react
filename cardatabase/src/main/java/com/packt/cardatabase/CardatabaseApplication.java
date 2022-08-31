f15240c742ec1eba1e193c1ecb047a80096d9449
!Mensch <assassinste@gmail.com>
2022-08-31 17:10:28 +0800

    dev: Preload test data for Entity 'User' (for authentication)

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -13,6 +13,8 @@ import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
 import com.packt.cardatabase.domain.Owner;
 import com.packt.cardatabase.domain.OwnerRepository;
+import com.packt.cardatabase.domain.User;
+import com.packt.cardatabase.domain.UserRepository;
 
 import java.util.Arrays;
 
@@ -27,6 +29,9 @@ public class CardatabaseApplication implements CommandLineRunner {
 
 	@Autowired
 	private OwnerRepository ownerRepository;
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CardatabaseApplication.class, args);
@@ -48,5 +53,18 @@ public class CardatabaseApplication implements CommandLineRunner {
 		carRepository.save(new Car("Eurus", "Eastwind", "Green", "MJK-3214", 41422, 2020, ownerAlex));
 		carRepository.save(new Car("Olive", "Anniversary", "White", "ERQ-2411", 15132, 2022, ownerMosh));
 		carRepository.save(new Car("Future", "TotalEV", "Black", "JOK-1242", 64235, 2020, ownerMosh));
+
+		// The passwords below are the same as the username (for the login process)
+		// I went to https://www.bcryptcalculator.com/encode to encode the credentials
+		userRepository.save(new User(
+				"user",
+				"$2a$10$nHZ.SGqmOMxIUbtob.THHOJwru.FLbfCNmTQUuq63ZeH6cgPTxeJe",
+				"USER"
+		));
+		userRepository.save(new User(
+				"admin",
+				"$2a$10$X3T9r0OqC0sYyShi0f3qnuDUcYVkefU0hw6dyQAqKGtr0//HwBzX2",
+				"ADMIN"
+		));
 	}
 }

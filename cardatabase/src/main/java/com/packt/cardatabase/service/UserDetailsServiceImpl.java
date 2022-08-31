88526e3ae5db22ab3ec62690b4d4fa404238141a
!Mensch <assassinste@gmail.com>
2022-08-31 17:09:27 +0800

    feat: Setup username for customized basic authentication

---
+++ cardatabase/src/main/java/com/packt/cardatabase/service/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package com.packt.cardatabase.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.packt.cardatabase.domain.User;
+import com.packt.cardatabase.domain.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Optional<User> user = userRepository.findByUsername(username);
+		UserBuilder userBuilder = null;
+
+		if (user.isPresent()) {
+			User currentUser = user.get();
+
+			userBuilder = org.springframework
+					.security.core
+					.userdetails.User.withUsername(username);
+
+			userBuilder.password(currentUser.getPassword());
+			userBuilder.roles(currentUser.getRole());
+		} else {
+			throw new UsernameNotFoundException("User not found.");
+		}
+
+		return userBuilder.build();
+	}
+}

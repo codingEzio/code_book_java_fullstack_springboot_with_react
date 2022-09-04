238d82eabc541df199eb597ceecf8dcdfda7c60e
!Mensch <assassinste@gmail.com>
2022-09-04 08:41:02 +0800

    test: Checking if the CarController could be initialized as expected

--- cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
+++ cardatabase/src/test/java/com/packt/cardatabase/CardatabaseApplicationTests.java
@@ -1,13 +1,25 @@
 package com.packt.cardatabase;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.packt.cardatabase.web.CarController;
 
 @SpringBootTest
 class CardatabaseApplicationTests {
 
+	@Autowired
+	private CarController carController;
+
 	@Test
+	@DisplayName("First example test for Car Controller")
 	void contextLoads() {
+		assertThat(carController).isNotNull();
 	}
 
 }

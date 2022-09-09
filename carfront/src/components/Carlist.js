356bfb12ccf2ed5048c53d1ce999540f70dc4226
!Mensch <assassinste@gmail.com>
2022-09-09 11:10:58 +0800

    feat: Implement READ for frontend CRUD functionalities

--- carfront/src/components/Carlist.js
+++ carfront/src/components/Carlist.js
@@ -1,8 +1,17 @@
 import React, { useState, useEffect } from 'react';
+import { DataGrid } from '@mui/x-data-grid';
+
 import { SERVER_URL } from '../constants';
 
 const Carlist = () => {
   const [cars, setCars] = useState([]);
+  const datagrid_columns = [
+    { field: 'brand', headerName: 'Brand', width: 200 },
+    { field: 'model', headerName: 'Model', width: 200 },
+    { field: 'color', headerName: 'Color', width: 200 },
+    { field: 'releasedAt', headerName: 'Released At', width: 200 },
+    { field: 'price', headerName: 'Price', width: 200 },
+  ];
 
   useEffect(() => {
     fetch(SERVER_URL + 'api/cars')
@@ -12,20 +21,12 @@ const Carlist = () => {
   });
 
   return (
-    <div>
-      <table>
-        <tbody>
-          {cars.map((car, index) => (
-            <tr key={index}>
-              <td>{car.brand}</td>
-              <td>{car.model}</td>
-              <td>{car.released_at}</td>
-              <td>{car.color}</td>
-              <td>{car.price}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div style={{ height: 500, width: '100%' }}>
+      <DataGrid
+        rows={cars}
+        columns={datagrid_columns}
+        getRowId={row => row._links.self.href}
+      />
     </div>
   );
 };

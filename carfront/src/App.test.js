891cfd710d25521003423c11c82da72060d64920
!Mensch <assassinste@gmail.com>
2022-09-10 18:00:19 +0800

    test: Testing if the homepage has text 'New car' in it

--- carfront/src/App.test.js
+++ carfront/src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders buttons and tables which has "new car" in it', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/new car/i);
   expect(linkElement).toBeInTheDocument();
 });

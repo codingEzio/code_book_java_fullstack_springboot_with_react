cd4169ddb0c5c6a6c5c407573e65757794be7266
!Mensch <assassinste@gmail.com>
2022-09-10 18:08:57 +0800

    test: Testing if there's text 'New car' in the model form

--- carfront/src/App.test.js
+++ carfront/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders buttons and tables which has "new car" in it', () => {
   render(<App />);
   const linkElement = screen.getByText(/new car/i);
+
   expect(linkElement).toBeInTheDocument();
+});
+
+test('open add car model form', async () => {
+  render(<App />);
+  fireEvent.click(screen.getByText('New Car'));
+
+  expect(screen.getByRole('dialog')).toHaveTextContent('New car');
 });
